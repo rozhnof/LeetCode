9e58c0786847e646ff024f409080d6acfbb39727
rozhnof <rozhnoff.vv@gmail.com>
2024-02-29 01:08:11 +0700

    feat: solved 25

--- src/25_Reverse_Nodes_in_k-Group.cc
+++ src/25_Reverse_Nodes_in_k-Group.cc
@@ -17,45 +17,51 @@ class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
         int group_count = 0;
-        for (auto node = head; node->next; node = node->next) {
+        for (auto node = head; node; node = node->next) {
             ++group_count;
         }
         group_count /= k;
 
-        // if (k > 0) {
-            auto [head_first, head_last] = reverseList(head, k);
-            head = head_first;
+        if (group_count > 0) {
+            auto [new_head, old_head] = reverseList(head, k);
+            head = new_head;
 
-            // for (int i = 1; i < k; ++i) {
-                // auto [first, last] = reverseList(head_last->next, k);
-                // head_last->next = first;
-                // head_last = last;
-            // }
-        // }
+            for (int i = 1; i < group_count; ++i) {
+                auto [new_head_i, old_head_i] = reverseList(old_head->next, k);
+                old_head->next = new_head_i;
+                old_head = old_head_i;
+            }
+        }
         return head;
     }
 
     pair<ListNode*, ListNode*> reverseList(ListNode* head, int k) {
-        auto prev = head;
-        auto cur = head->next;
-        prev->next = nullptr;
-        while (k >= 0) {
-            auto next = cur->next;
-            cur->next = prev;
-            prev = cur;
-            cur = next;
-            --k;
-        }
-        cur->next = prev;
+        auto [new_head, old_head] = reverse(nullptr, head, k - 1);
+        head->next = old_head;
+        return {new_head, head};
+    }
+   
+    pair<ListNode*, ListNode*> reverse(ListNode* prev, ListNode*& head, int k) {
+        ListNode* new_head = head;
+        ListNode* next = head->next;
 
-        return {cur, head};
+        if (k > 0) {
+            auto [new_head_1, next_1] = reverse(head, head->next, k - 1);
+            new_head = new_head_1;
+            next = next_1;
+        } 
+        
+        head->next = prev;
+        return {new_head, next};
     }
 };
 
 int main() {
     Solution s;
 
-    ListNode* list1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+    ListNode* list1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))));
+    // ListNode* list1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+    // ListNode* list1 = new ListNode(1, new ListNode(2));
 
     auto res = s.reverseKGroup(list1, 2);
 
